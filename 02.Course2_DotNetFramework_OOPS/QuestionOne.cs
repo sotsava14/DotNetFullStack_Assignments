c0578d2cfdd9f1f5d08be692c37e6903553281e1
Sotsava Skandhaa <85110593+sotsava14@users.noreply.github.com>
2024-09-03 22:24:55 +0530

    Add files via upload

---
+++ 02.Course2_DotNetFramework_OOPS/QuestionOne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class QuestionOne
+    {
+        static void Main(string[] args)
+        {
+            string uname,salary;
+            string id,job;
+            
+
+            Console.WriteLine("Enter your ID : ");
+            id = Console.ReadLine();
+
+            Console.WriteLine("Enter your Name : ");
+            uname=Console.ReadLine();
+            
+            Console.WriteLine("Enter your Job : ");
+            job = Console.ReadLine();
+
+            Console.WriteLine("Enter your Salary : ");
+            salary = Console.ReadLine();
+
+            Console.WriteLine("--------User Details-------");
+
+            Console.WriteLine("Employee ID : " + id);
+            Console.WriteLine("Employee Name : " + uname);
+            Console.WriteLine("Employee Job : " + job);
+            Console.WriteLine("Employee Salary : " + salary);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
