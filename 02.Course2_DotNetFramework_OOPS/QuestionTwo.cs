f944f609ec78c03c4e736374998c666ed0c4e6a2
Sotsava Skandhaa <85110593+sotsava14@users.noreply.github.com>
2024-09-04 09:43:38 +0530

    Add files via upload

---
+++ 02.Course2_DotNetFramework_OOPS/QuestionTwo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class QuestionTwo
+    {
+        static void Main(string[] args)
+        {
+            int pID;
+            string pName;
+            int x;
+            int y;
+            double total;
+            double discount;
+            double final;
+
+            Console.WriteLine("Enter Product ID : ");
+            pID = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Product Name : ");
+            Product_Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Unit Price : ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Quantity : ");
+            y = int.Parse(Console.ReadLine());
+
+            total = x * y;
+
+            if (y <= 10)
+            {
+                discount = 0;
+                final = total;
+                Console.WriteLine("Total Amount : " + total);
+                Console.WriteLine("Discount Amount : " + discount);
+                Console.WriteLine("Final Amount : " + final);
+            }
+            else if (y > 10 && y <= 30)
+            {
+                discount = 0.1 * total;
+                final = total - (discount);
+                Console.WriteLine("Total Amount : " + total);
+                Console.WriteLine("Discount Amount : " + discount);
+                Console.WriteLine("Final Amount : " + final);
+            }
+            else if (y > 30 && y <= 50)
+            {
+                discount = 0.2 * total;
+                final = total - (discount);
+                Console.WriteLine("Total Amount : " + total);
+                Console.WriteLine("Discount Amount : " + discount);
+                Console.WriteLine("Final Amount : " + final);
+            }
+            else if (y > 50)
+            {
+                discount = 0.3 * total;
+                final = total - (discount);
+                Console.WriteLine("Total Amount : " + total);
+                Console.WriteLine("Discount Amount : " + discount);
+                Console.WriteLine("Final Amount : " + final);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
