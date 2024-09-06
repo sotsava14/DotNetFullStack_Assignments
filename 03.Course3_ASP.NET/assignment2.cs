2a5f6108728a5ae253a13027f31d824a13873dd7
Sotsava Skandhaa <85110593+sotsava14@users.noreply.github.com>
2024-09-06 11:45:32 +0530

    Add files via upload

---
+++ 03.Course3_ASP.NET/assignment2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "Server = USHYDDEPRANEET1; Database=Sept2024Db; Integrated Security=true";
+
+            SqlConnection conn = new SqlConnection(connStr);
+            // conn.ConnectionString = connStr;
+
+            string cmdText = "INSERT INTO Product VALUES(7,'DOLO',10,2,'MEDICINE')";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+
+            conn.Open();
+            Console.WriteLine("Connected to Database successfully");
+
+            int n = cmd.ExecuteNonQuery();  // For DML:  Insert, update, delete
+
+            Console.WriteLine("No. of rows affected :" + n);
+            conn.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03.Course3_ASP.NET/assignment3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "Server = USHYDDEPRANEET1; Database=Sept2024Db; Integrated Security=true";
+
+            string cmdText = "SELECT * FROM Product";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmdText, connStr);
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds);
+
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                Console.WriteLine("{0},{1},{2},{3},{4}", row["ProductID"].ToString(), row["ProductName"].ToString(), row["Price"].ToString(), row["Quantity"].ToString(), row["Category"].ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
