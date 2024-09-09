8f445543b4e90a6c96476bfafb30396e30752490
Sotsava Skandhaa <85110593+sotsava14@users.noreply.github.com>
2024-09-09 12:54:18 +0530

    Add files via upload

---
+++ 03.Course3_WebApplication_ASP.NET/03.Module3_EntityFramework/WebApplication2/WebApplication2/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class SalesDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public SalesDbContext(DbContextOptions<SalesDbContext> options)
+         : base(options)
+        {
+
+        }
+    }
+}
