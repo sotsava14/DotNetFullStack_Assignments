fcf50c7a341c337f50ff4bd97e661fa844df7072
Sotsava Skandhaa <85110593+sotsava14@users.noreply.github.com>
2024-09-12 10:54:02 +0530

    Add files via upload

---
+++ 05.Course5_Advanced_DotNetCore/02.Module2_Web_API/Assignment2/WebApplication4/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication4.Models;
+namespace WebApplication4.Repositories
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAllProducts();
+        void CreateProduct(Product obj);
+        Product GetProductById(int id);
+        void EditProduct(Product obj);
+        string DeleteProductById(int id);
+    }
+}
